629bb6a5e79d474c6f6b08e30c49de22f4e4999b
cchen13-rgb <cchen13@risd.edu>
2025-10-07 15:08:03 -0400

    10/7

---
+++ myscript.js
@@ -0,0 +1,12 @@
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  
+  const myButton = document.querySelector("button");
+
+  myButton.addEventListener("click", (event) => {
+    alert('Hello World!');
+  });
+  
+});
+
+
