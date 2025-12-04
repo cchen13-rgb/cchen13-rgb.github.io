e2e89451e0d078ddbc2c9dd6974546f52fa095f1
cchen13-rgb <cchen13@risd.edu>
2025-12-04 01:44:52 -0500

    Changes 12/4

---
+++ on-and-off/myscript.js
@@ -0,0 +1,17 @@
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  
+  const myButton = document.querySelector("button");
+
+  myButton.addEventListener("click", (event) => {
+    alert('Hello World!');
+  });
+  
+  const toggle = document.querySelector("Togglefull")
+
+  toggle.addEventListener("click",(event)=> { 
+    
+  })
+});
+
+
